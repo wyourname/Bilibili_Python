4dce3d8da9bf4cba85d0e6b0ac3bf0072e9cb23a
wangquanfugui233 <63834404+wangquanfugui233@users.noreply.github.com>
2023-01-30 20:52:55 +0800

    Update Dev_Bilibili_User.py
    翻下身

--- Bilibili_Dev/Dev_Bilibili_User.py
+++ Bilibili_Dev/Dev_Bilibili_User.py
@@ -1,12 +1,15 @@
 """
-new Env("哔哩哔哩dev-【basic】")
-cron : 2 1 * * *
+cron: 2 1 * * *
+new Env("哔哩哔哩-【基础检查】")
 """
 
 import json
 import random
 import time
-from Dev_Bilibili_Config import Config
+
+from urllib3.exceptions import ConnectTimeoutError
+
+from Bilibili_Config import Config
 import requests
 
 
@@ -17,13 +20,20 @@ class Basic(Config):
         self.csrfs = self.fetch_csrf(self.cookies)
         self.coin = self.fetch_drop_coin()
 
-    def get_requests(self, url):
+    def get_requests(self, url, proxy=None):
         try:
             time.sleep(1)
             self.headers['user-agent'] = random.choice(self.ua_list)
             with requests.session() as s:
+                # 代理
                 s.keep_alive = False
-                r = s.get(url, headers=self.headers)
+                s.adapters.DEFAULT_RETRIES = 2
+                if proxy is None:
+                    # proxies = {'http': f'http://{proxy}'}
+                    r = s.get(url, headers=self.headers)
+                else:
+                    proxies = {'http': f'http://{proxy}'}
+                    r = s.get(url, headers=self.headers, proxies=proxies)
                 if r.status_code == 200:
                     data = json.loads(r.text)
                     return data
@@ -33,14 +43,18 @@ class Basic(Config):
         except Exception as e:
             self.logger.error('请求失败，错误信息：{}'.format(e))
 
-    def post_requests(self, url, data):
+    def post_requests(self, url, data, proxy=None):
         try:
             self.headers['method'] = 'POST'
             self.headers['Content-Type'] = 'application/x-www-form-urlencoded'
             time.sleep(1)
             with requests.session() as s:
                 s.keep_alive = False
-                r = s.post(url, headers=self.headers, data=data)
+                if proxy is None:
+                    r = s.post(url, headers=self.headers, data=data)
+                else:
+                    proxies = {'http': f'http://{proxy}'}
+                    r = s.post(url, headers=self.headers, data=data, proxies=proxies)
                 if r.status_code == 200:
                     post_data = json.loads(r.text)
                     return post_data
@@ -61,11 +75,11 @@ class Basic(Config):
         else:
             return False
 
-    def follow_author(self, csrf):
-        data = {'fid': 289549318, 'act': 1, 're_src': 11, 'csrf': csrf}
+    def unfollow_author(self, csrf):
+        data = {'fid': 289549318, 'act': 2, 're_src': 11, 'csrf': csrf}
         follow = self.post_requests(self.url1, data)
         if follow['code'] == 0:
-            self.logger.info("关注作者成功")
+            self.logger.info("取关作者成功")
         else:
             self.logger.info(follow['message'])
 
@@ -77,9 +91,12 @@ class Basic(Config):
                 self.logger.info("你已经是lv6的大佬了")
             else:
                 level_day = (data['data']['level_info']['next_exp'] - data['data']['level_info']['current_exp']) / 65
-                self.logger.info('当前硬币数：' + str(data['data']['money']) + "，下一等级升级天数约" + str(int(level_day)))
+                self.logger.info(
+                    '当前硬币数：' + str(data['data']['money']) + "，下一等级升级天数约" + str(int(level_day)))
+            return True
         elif data['code'] == -101:
             self.logger.info(data['message'] + "请检查cookie")
+            return False
         elif data['code'] == -111:
             self.logger.info(data['message'] + "请检查csrf")
         else:
@@ -88,25 +105,57 @@ class Basic(Config):
     def mao_san(self, csrf):
         if self.fetch_follow():
             if self.check_author():
-                pass
-            else:
-                self.follow_author(csrf)
-        else:
-            pass
+                self.unfollow_author(csrf)
 
-    def manual(self):
-        self.logger.info("author: github@wangquanfugui233")
-        self.logger.info("本次更新加入关注作者B站号，将随机为作者提供0.1个硬币")
-        self.logger.info("这让你感到不适，请删除脚本或者配置文件将follow_author设置为小写false")
+    def check_proxy(self):
+        proxys = self.fetch_proxy()
+        if not proxys:
+            self.logger.info("不使用代理")
+        else:
+            for i in proxys:
+                if self.result_proxy(i):
+                    self.logger.info(f"{i}该代理是ok的，保留下来")
+                else:
+                    # self.remove_proxy(i)  暂时不动这里
+                    pass
+
+    def result_proxy(self, ip):
+        try:
+            requests.adapters.DEFAULT_RETRIES = 3
+            this_proxy = "http://" + ip
+            res = requests.get(url="http://icanhazip.com/", timeout=8, proxies={"http": this_proxy})
+            if res.status_code == 200:
+                return True
+        except Exception:
+            self.logger.error(f"该项检测的代理{ip}无效，响应时间过长")
+            return False
+
+    def proxy_num(self):
+        proxy_a = self.fetch_proxy()
+        if proxy_a is not None:
+            return proxy_a
+        else:
+            return None
 
     def user_info(self):
-        self.manual()
+        self.logger.info("更新：将已关注我的取关硬币将够，加入代理（未测试），感觉鸡肋")
+        self.check_proxy()
+        p_list = self.proxy_num()
         for i in range(len(self.cookies)):
             self.headers['Cookie'] = self.cookies[i]
-            user = self.get_requests(self.url)
-            self.cope_info(user)
+            if not p_list:
+                user = self.get_requests(self.url)
+                self.cope_info(user)
+                self.mao_san(self.csrfs[i])
+            else:
+                # p = random.choice(p_list)
+                p = random.choice(p_list)
+                user = self.get_requests(self.url, p)
+                self.cope_info(user)
+                self.mao_san(self.csrfs[i])
 
 
 if __name__ == '__main__':
     cope = Basic()
     cope.user_info()
+    # cope.check_proxy()
