ea3a33565ac10a45f055e73284066f3752333500
wangquanfugui233 <63834404+wangquanfugui233@users.noreply.github.com>
2023-06-29 21:33:27 +0800

    更改配置文件路径

--- Basic/config.py
+++ Basic/config.py
@@ -57,7 +57,7 @@ class Config:
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 "
             "Safari/537.36 "
         ]
-        self.config_dir = "../config.yaml"
+        self.config_dir = "./config.yaml"
         logging.basicConfig(level=logging.INFO, format='%(message)s')
         self.logger = logging.getLogger(__name__)
 
